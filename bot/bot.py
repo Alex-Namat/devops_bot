98e128c51fb7eac08d7b6d7450aa586c97851504
Alex-Namat <alex.namat.m@gmail.com>
2024-05-12 15:59:11 -0400

    logging from bot

--- bot/bot.py
+++ bot/bot.py
@@ -39,7 +39,7 @@ from commands import(
 
 # Enable logging
 logging.basicConfig(
-    filename='logfile.txt', format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 # set higher logging level for httpx to avoid all GET and POST requests being logged
 logging.getLogger("httpx").setLevel(logging.WARNING)
