dfa875d9829f0f1c96c8f33ecd4a35bdd54d8adc
Alex-Namat <alex.namat.m@gmail.com>
2024-05-17 20:27:25 -0400

    replica working

--- db/init.sql
+++ db/init.sql
@@ -14,13 +14,21 @@ INSERT INTO phones (phone) VALUES ('89162345678');
 
 \set DB_REPL_USER `echo "${DB_REPL_USER}"`
 \set DB_REPL_PASSWORD `echo "${DB_REPL_PASSWORD}"`
-CREATE ROLE :DB_REPL_USER WITH REPLICATION ENCRYPTED PASSWORD :'DB_REPL_PASSWORD';
-SELECT pg_create_physical_replication_slot('replication_slot');
+CREATE USER :DB_REPL_USER WITH REPLICATION ENCRYPTED PASSWORD :'DB_REPL_PASSWORD';
 
 \set DB_REPL_HOST `echo "${DB_REPL_HOST}"`
 CREATE TABLE hba ( lines text ); 
 COPY hba FROM '/var/lib/postgresql/data/pg_hba.conf';
 \set val 'host replication '  :DB_REPL_USER  ' 0.0.0.0/0 scram-sha-256'
 INSERT INTO hba (lines) VALUES (:'val');
-COPY hba TO '/var/lib/postgresql/data/pg_hba.conf'; 
+COPY hba TO '/var/lib/postgresql/data/pg_hba.conf';
+
+ALTER SYSTEM SET logging_collector = on;
+ALTER SYSTEM SET log_replication_commands = on;
+ALTER SYSTEM SET wal_level = replica;
+ALTER SYSTEM SET hot_standby = on;
+ALTER SYSTEM SET max_wal_senders = 10;
+ALTER SYSTEM SET max_replication_slots = 10;
+ALTER SYSTEM SET hot_standby_feedback = on;
+
 SELECT pg_reload_conf();
