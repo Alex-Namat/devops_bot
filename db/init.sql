f78aa8410d410681182c2d45cc793b20a0dc6590
Alex-Namat <alex.namat.m@gmail.com>
2024-05-18 18:10:14 -0400

    refactoring

--- db/init.sql
+++ db/init.sql
@@ -16,19 +16,18 @@ INSERT INTO phones (phone) VALUES ('89162345678');
 \set DB_REPL_PASSWORD `echo "${DB_REPL_PASSWORD}"`
 CREATE USER :DB_REPL_USER WITH REPLICATION ENCRYPTED PASSWORD :'DB_REPL_PASSWORD';
 
-\set DB_REPL_HOST `echo "${DB_REPL_HOST}"`
 CREATE TABLE hba ( lines text ); 
 COPY hba FROM '/var/lib/postgresql/data/pg_hba.conf';
-\set val 'host replication '  :DB_REPL_USER  ' 0.0.0.0/0 scram-sha-256'
+\set val 'host replication '  :DB_REPL_USER  ' samenet scram-sha-256'
 INSERT INTO hba (lines) VALUES (:'val');
 COPY hba TO '/var/lib/postgresql/data/pg_hba.conf';
 
 ALTER SYSTEM SET logging_collector = on;
 ALTER SYSTEM SET log_replication_commands = on;
-ALTER SYSTEM SET wal_level = replica;
-ALTER SYSTEM SET hot_standby = on;
+ALTER SYSTEM SET archive_mode = on;
+ALTER SYSTEM SET archive_command = 'cp %p /oracle/pg_data/archive/%f';
 ALTER SYSTEM SET max_wal_senders = 10;
-ALTER SYSTEM SET max_replication_slots = 10;
-ALTER SYSTEM SET hot_standby_feedback = on;
+ALTER SYSTEM SET wal_level = replica;
+ALTER SYSTEM SET wal_log_hints = on;
 
 SELECT pg_reload_conf();
